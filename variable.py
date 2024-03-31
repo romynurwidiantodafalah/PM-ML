7bd139d8073788a5514b611e17515a4499da3f10
Romy <110410235+romynurwidiantodafalah@users.noreply.github.com>
2024-03-31 19:33:52 +0700

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,34 @@
+## Latihan: Variables
+
+# <b>Cerita Pendek:</b> 
+
+# Suatu waktu di kebun apel, Darjo memiliki 3 apel, Darno memiliki 5 apel dan Waginem memiliki 6 Apel. mereka sangat bahagia dan hidup lama. selesai.
+
+# Tugas Anda
+# - buat variabe : `Darjo`, `Darno` dan `Waginem`
+# - isi nilai sesuai dengan jumlah apel yang mereka miliki
+# - cetak nama variabel dan jumlah apel setiap variabel dalam 1 baris, pisahkan dengan koma
+# - buat variabel baru dengan nama `totalApel` yang berisi penjumlahan seluruh apel yang mereka miliki
+# - coba otak atik code dengan membuat variable baru, diisi dengan nilai lain, dan dihitung dengan operator aritmetik lainnya
+
+Darjo = 3
+Darno = 5
+Waginem = 6
+print(f"Darjo: {Darjo}, Darno: {Darno}, Waginem: {Waginem}\n")
+
+# TOTAL APEL
+totalApel = Darjo + Darno + Waginem
+print(f"Total apel: {totalApel}\n")
+
+# PENDAPATAN
+hargaApel = 5000
+totalPendapatan = totalApel * hargaApel
+print(f"Total pendapatan: {totalPendapatan}\n")
+
+# PEMBAGIAN
+sisaApel = totalApel % 10
+print(f"Sisa apel: {sisaApel}\n")
+
+# PANGKAT
+pangkatApel = totalApel ** 2
+print(f"Kelipatan total apel: {pangkatApel}\n")
